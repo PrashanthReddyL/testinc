17f794d13de6aa1767486906b4d72224c65cca0e
Prashanth Reddy <lprashanthreddy9@gmail.com>
2024-03-27 21:32:16 -0500

    tilte change test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test App for Azure
+          Test App for Azure..!
         </p>
       </header>
     </div>
